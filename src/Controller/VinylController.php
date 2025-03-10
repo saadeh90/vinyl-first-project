0be94115eda49e65fcdfe97de5d748b51dcedffd
bilal saadeh <bila.saade@gmail.com>
2025-03-10 09:19:58 +0200

    adding twig

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -2,7 +2,24 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use function Symfony\Component\String\u;
+
 class VinylController
 {
+    #[Route('/vinyl', name: 'app_vinyl_homepage')]
+    public function homepage()
+    {
+        return new Response('Welcome to Vinyl');
+    }
 
+    #[Route('/browse/{slug}', name: 'app_vinyl_browse')]
+    public function browse($slug)
+    {
+        $title = $slug ? u(str_replace('-', ' ', $slug))->title(true) : null;
+
+        return
+
+    }
 }
